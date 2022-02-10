68b0d65c615bae0daa84147b5eac2ec6d69a4c7b
Airvisual Creative <airvisualcreative@gmail.com>
2022-02-10 22:29:57 +0700

    adding html lang

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar";
+import Document, { Html } from 'next/document'
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
@@ -9,9 +10,15 @@ import FeaturesTwo from "../components/FeaturesSectionTwo";
 export default function Home() {
   return(
     <>
+    <Html lang="en"></Html>
     <Head>
         <title>Test SEO Performance</title>
         <meta property="og:title" content="My page title" key="title" />
+        <meta name="description" content="Airvisual Creative Studio,  The Other, Digital Marketing 
+        Agency from Bandung, 
+        West Java, Indonesia">
+          
+        </meta>
       </Head>
       <Head>
         <meta property="og:title" content="My new title" key="title" />
